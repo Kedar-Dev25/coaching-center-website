7fcf02a66e08cd4b556cbb9d27683ebf28eab658
Kedar-Dev25 <kedar.code7@gmail.com>
2026-06-29 22:19:15 +0530

    Navbar Completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,6 @@
 function Hero() {
   return (
-    <section id="home">
-      <h1>Empowering Minds, Shaping Futures</h1>
-      <p>
-        Personalized coaching and academic excellence for every student.
-      </p>
+    <section className="hero">
     </section>
   );
 }
